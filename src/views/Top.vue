0f3b6445156fa784c9c6b9a1b3c1428b66061c9d
HiroKb <60965041+HiroKb@users.noreply.github.com>
2020-07-01 02:36:50 +0900

    add granim.jsの追加・TOPページ作成

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,9 +1,70 @@
 <template>
-    <h1>Home</h1>
+    <v-container>
+        <div class="bloc-logo">
+            <canvas id="logo-canvas"></canvas>
+            <h1 class="logo-mask">Hiro'sPortfolio</h1>
+        </div>
+    </v-container>
 </template>
 
 <script>
+    import Granim from 'granim'
     export default {
         name: 'Top',
+        data () {
+            return {
+                granim: {}
+            }
+        },
+        mounted() {
+            this.granim = new Granim({
+                element: '#logo-canvas',
+                direction: 'left-right',
+                states : {
+                    "default-state": {
+                        gradients: [
+                            ['#EB3349', '#F45C43'],
+                            ['#FF8008', '#FFC837'],
+                            ['#4CB8C4', '#3CD3AD'],
+                            ['#24C6DC', '#514A9D'],
+                            ['#FF512F', '#DD2476'],
+                            ['#DA22FF', '#9733EE']
+                        ],
+                        transitionSpeed: 2000
+                    }
+                }
+            })
+        }
     }
 </script>
+
+<style scoped>
+    .container{
+        height: 100%;
+        display: flex;
+        align-items: center;
+    }
+    .bloc-logo {
+        position: relative;
+        width: 382px;
+        height: 198px;
+        float: left;
+    }
+
+    .bloc-logo canvas,
+    .bloc-logo .logo-mask {
+        display: block;
+        width: 382px;
+        height: 198px;
+    }
+
+    .bloc-logo .logo-mask {
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-size: 400px;
+        background-image: url("../assets/topimage.png");
+        text-indent: -9999px;
+    }
+</style>
+
