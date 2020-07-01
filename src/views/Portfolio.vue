c853014c5d1ccd16fa7ff1b2dec6847dae5d5b8d
HiroKb <60965041+HiroKb@users.noreply.github.com>
2020-07-02 01:18:42 +0900

    update Portfolioページレスポンシブ対応

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -4,11 +4,11 @@
 
             <div class="portfolio__item d-flex flex-column align-center">
                 <section-title>ShareCalendar</section-title>
-                <v-row class="mt-4" style="width: 100%">
-                    <v-col cols="6" class="elevation-2">
+                <v-row class="mt-sm-4 mt-2" style="width: 100%">
+                    <v-col cols="sm-6 12" class="elevation-2">
                         <v-img src="../assets/share_calendar.png"></v-img>
                     </v-col>
-                    <v-col cols="6" class="d-flex justify-center align-center">
+                    <v-col cols="sm-6 12" class="d-flex justify-center align-center">
                         <div class="d-flex flex-column align-center">
                             <h3>シンプルなスケジュール共有SPAです</h3>
                             <div class="mt-4">
@@ -25,8 +25,11 @@
 
             <div class="portfolio__item d-flex flex-column align-center">
                 <section-title>Portfolio</section-title>
-                <v-row class="mt-4" style="width: 100%">
-                    <v-col cols="6" class="d-flex justify-center align-center">
+                <v-row class="mt-sm-4 mt-2" style="width: 100%">
+                    <v-col cols="sm-6 12" order-sm="2" class="elevation-2">
+                        <v-img src="../assets/portfolio.png"></v-img>
+                    </v-col>
+                    <v-col cols="sm-6 12" order-sm="1" class="d-flex justify-center align-center">
                         <div class="d-flex flex-column align-center">
                             <h3>当ポートフォリオです</h3>
                             <div class="mt-4">
@@ -34,9 +37,6 @@
                             </div>
                             <p class="font-weight-bold mt-4">Code: <a href="https://github.com/HiroKb/portfolio">GitHub</a></p>
                         </div>
-                    </v-col>
-                    <v-col cols="6" class="elevation-2">
-                        <v-img src="../assets/portfolio.png"></v-img>
                     </v-col>
                 </v-row>
             </div>
@@ -68,5 +68,11 @@
         font-size: 18px;
         color: gray;
         opacity: .8;
+    }
+
+    @media screen and (max-width: 599px){
+        .portfolio{
+            margin-top: 80px;
+        }
     }
 </style>
