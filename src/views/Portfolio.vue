a062167eab6ee3c9ba5f05e0ad28bfb12f671f12
HiroKb <60965041+HiroKb@users.noreply.github.com>
2020-07-02 21:14:57 +0900

    update ポートフォリオ追加

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -10,7 +10,7 @@
                     </v-col>
                     <v-col cols="sm-6 12" class="d-flex justify-center align-center">
                         <div class="d-flex flex-column align-center">
-                            <h3>シンプルなスケジュール共有SPAです</h3>
+                            <h3>シンプルなスケジュール共有SPA</h3>
                             <div class="mt-4">
                                 <skill-chip color="#41B882">Vue.js</skill-chip>
                                 <skill-chip color="#F72C21" class="ml-2">Laravel</skill-chip>
@@ -31,15 +31,58 @@
                     </v-col>
                     <v-col cols="sm-6 12" order-sm="1" class="d-flex justify-center align-center">
                         <div class="d-flex flex-column align-center">
-                            <h3>当ポートフォリオです</h3>
+                            <h3>当ポートフォリオ</h3>
                             <div class="mt-4">
                                 <skill-chip color="#41B882">Vue.js</skill-chip>
+                                <skill-chip color="#7BC5FF" class="ml-2">Vuetify</skill-chip>
                             </div>
                             <p class="font-weight-bold mt-4">Code: <a href="https://github.com/HiroKb/portfolio">GitHub</a></p>
                         </div>
                     </v-col>
                 </v-row>
             </div>
+
+            <div class="portfolio__item d-flex flex-column align-center">
+                <section-title>CafePage</section-title>
+                <v-row class="mt-sm-4 mt-2" style="width: 100%">
+                    <v-col cols="sm-6 12" class="elevation-2">
+                        <v-img src="../assets/k's_cafe.jpg"></v-img>
+                    </v-col>
+                    <v-col cols="sm-6 12" class="d-flex justify-center align-center">
+                        <div class="d-flex flex-column align-center">
+                            <h3>架空のカフェホームページ</h3>
+                            <div class="mt-4">
+                                <skill-chip color="#CF649A">Sass</skill-chip>
+                                <skill-chip color="#21609B" class="ml-2">jQuery</skill-chip>
+                                <skill-chip color="#D34A47" class="ml-2">gulp</skill-chip>
+                            </div>
+                            <p class="font-weight-bold mt-4 mb-2">Link: <a href="https://sample1.hirokb.com">CafePage</a></p>
+                            <p class="font-weight-bold">Code/Infra: <a href="https://github.com/HiroKb/fictional-cafe">GitHub</a></p>
+                        </div>
+                    </v-col>
+                </v-row>
+            </div>
+
+            <div class="portfolio__item d-flex flex-column align-center">
+                <section-title>OrgTas</section-title>
+                <v-row class="mt-sm-4 mt-2" style="width: 100%">
+                    <v-col cols="sm-6 12" order-sm="2" class="elevation-2">
+                        <v-img src="../assets/orgtas.png"></v-img>
+                    </v-col>
+                    <v-col cols="sm-6 12" order-sm="1" class="d-flex justify-center align-center">
+                        <div class="d-flex flex-column align-center">
+                            <h3>タスク共有アプリ</h3>
+                            <div class="mt-4">
+                                <skill-chip color="#6181B6">PHP</skill-chip>
+                                <skill-chip color="#21609B" class="ml-2">jQuery</skill-chip>
+                            </div>
+                            <p class="font-weight-bold mt-4 mb-2">Link: 未デプロイ</p>
+                            <p class="font-weight-bold">Code: <a href="https://github.com/HiroKb/orgtas">GitHub</a></p>
+                        </div>
+                    </v-col>
+                </v-row>
+            </div>
+            
         </div>
     </v-container>
 </template>
